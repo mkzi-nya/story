02787b4fc05087aa1f5ba835c34e51e25da6092c
mkzi_nya <wcnmngsb999@gamil.com>
2025-10-20 14:33:45 +0800

    meow

--- vitepress/docs/.vitepress/config.js
+++ vitepress/docs/.vitepress/config.js
@@ -16,7 +16,7 @@ export default defineConfig({
       },
       {
         text: '其他文章',
-        items: [{ text: 'Milthm 社区衍生内容创作守则', link: 'https://milthm.com/agreement/derivative/' },{ text: 'Milthm 社区衍生内容创作守则 常见问题解答', link: 'https://milthm.com/wiki/hans/faq/derivative_policy' }]
+        items: [{ text: 'Milthm 社区衍生内容创作守则', link: 'https://www.milthm.cn/agreement/derivative/' },{ text: 'Milthm 社区衍生内容创作守则 常见问题解答', link: 'https://milthm.com/wiki/hans/faq/derivative_policy' }]
       }    ],
     outline: { level: [2, 4] },
     outlineTitle: 'On this page'
