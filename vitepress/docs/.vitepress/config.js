416a3983c2d4a0b5c4ca1b3a58d99b31c12d3ca1
mkzi_nya <wcnmngsb999@gamil.com>
2025-10-20 16:21:24 +0800

    meow

--- vitepress/docs/.vitepress/config.js
+++ vitepress/docs/.vitepress/config.js
@@ -9,15 +9,30 @@ export default defineConfig({
   themeConfig: {
     nav: [{ text: 'Home', link: '/index' }],
     sidebar: [
-      { text: 'Milthm Story', items: [{ text: 'Languages', link: '/story' }] },
+      { 
+        text: 'Milthm Story', 
+        items: [{ text: 'Languages', link: '/story' }] 
+      },
       {
         text: '二创分区',
-        items: [{ text: '曲绘分析', link: '/illustration' }]
+        items: [
+          { text: '曲绘分析', link: '/illustration' },
+          {
+            text: '二创文章',
+            items: [
+              { text: '终焉', link: '/demise' },
+            ]
+          }
+        ]
       },
       {
         text: '其他文章',
-        items: [{ text: 'Milthm 社区衍生内容创作守则', link: 'https://www.milthm.cn/agreement/derivative/' },{ text: 'Milthm 社区衍生内容创作守则 常见问题解答', link: 'https://milthm.com/wiki/hans/faq/derivative_policy' }]
-      }    ],
+        items: [
+          { text: 'Milthm 社区衍生内容创作守则', link: 'https://www.milthm.cn/agreement/derivative/' },
+          { text: 'Milthm 社区衍生内容创作守则 常见问题解答', link: 'https://milthm.com/wiki/hans/faq/derivative_policy' }
+        ]
+      }
+    ],
     outline: { level: [2, 4] },
     outlineTitle: 'On this page'
   }
