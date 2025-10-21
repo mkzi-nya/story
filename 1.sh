490bd4dc971799c690bd95df6e7266b2577213e0
mkzi_nya <wcnmngsb999@gamil.com>
2025-10-21 09:05:53 +0800

    meow

--- 1.sh
+++ 1.sh
@@ -1,5 +1,6 @@
 cp -u -r "$(dirname "$0")/vitepress" ~/
 cd ~/vitepress
 pnpm run docs:build
+rm -rf "$(dirname "$0")"/assets
 cp -r ~/vitepress/docs/.vitepress/dist/* "$(dirname "$0")"/
 cd "$(dirname "$0")"
